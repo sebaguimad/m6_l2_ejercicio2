60bdfec7bad0b73122c19445a41e82953b8bd8cc
Sebastián Aguilera Madariaga <sebaguimad.94@gmail.com>
2023-02-08 04:29:27 -0300

    se agrega filtro por edad conductores sin vehiculo

--- db.js
+++ db.js
@@ -13,6 +13,7 @@ const client = new Client({
 
 client.connect(); 
 
+// Para conductores
 const traerConductores = ()=>{
     //para hacerlo funcionar tuve que typear la consulta en el query de postgres 
     // Se debe trabajar como una promesa porque era asíncrono
@@ -27,10 +28,43 @@ const traerConductores = ()=>{
     })
 }
 
+
 // llamando a la función
 //traerConductores();
 
+// Para vehiculos
+const traerVehiculos = () => {
+    //para hacerlo funcionar tuve que typear la consulta en el query de postgres 
+    // Se debe trabajar como una promesa porque era asíncrono
+    return new Promise((resolve, reject) => {
+        client.query("SELECT CONCAT(marca, ' - ',patente) vehiculo FROM automoviles;", (err, result) => {
+            if(err){
+                reject(err)
+            }
+            resolve(result.rows)
+        })
+    })
+}
+
+// Para traerConductoresSinVehiculo
+const traerConductoresSinVehiculo= (edad) => {
+    return new Promise((resolve, reject) => {
+        client.query(`select nombre, edad from conductores c
+                        left join automoviles a
+                        ON c.nombre = a.nombre_conductor
+                        where edad <= ${edad} and marca is null`, (err, result) => {
+            if(err){
+                return reject(err)
+            }
+            resolve(result.rows)
+        })
+    })
+}
+
+
 // exportamos la función para poder consumirla en index.js haciéndolo como ecmascript 5, en 6 se hace con import y export
 module.exports={
-    traerConductores
+    traerConductores,
+    traerVehiculos,
+    traerConductoresSinVehiculo
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 // destructuring se llaman los {}
-const { traerConductores } = require('./db.js')
+const { traerConductores, traerVehiculos, traerConductoresSinVehiculo } = require('./db.js')
 
 app.get('/', function (req, res) {
   res.send('Ejercicio 2')
@@ -23,7 +23,46 @@ app.get('/conductores', async function (req, res) {
     }
 })
   
+// RETORNA LISTA DE TODOS LOS VEHICULOS
+app.get('/vehiculos', async function(req, res) {
+    traerVehiculos().then(vehiculos => {
+        let acumulador = "<ol>"
+        vehiculos.forEach(vehiculo => {
+            console.log(vehiculo)
+            acumulador+= `<li>${vehiculo.vehiculo}</li>`
+        });
+        acumulador+="</ol>"
+        res.send(acumulador);
+
+    }).catch(error => {
+        res.send('no existen vehiculos en la BD.')
+    })
+  })
 
 
-  
+// RETORNA LISTA DE CONDUCTORES SIN AUTO
+
+app.get('/conductoressinauto', async function(req, res) {
+    let edad = req.query.edad || 99;
+    traerConductoresSinVehiculo(edad).then(listado => {
+
+        if(listado.length == 0){
+            return res.send(`<h1>No se encontraron usuarios sin autos con una edad inferior o igual a ${edad}</h1>`)
+        }
+        let acumulador = "<ol>"
+        listado.forEach(conductor => {
+            console.log(conductor )
+            acumulador+= `<li>Nombre: ${conductor.nombre} - Edad: ${conductor.edad}</li>`
+        });
+        acumulador+="</ol>"
+        res.send(acumulador);
+
+    }).catch(error => {
+        console.log(error)
+        res.status(500).send("Error interno del servidor.");
+    })
+
+  })
+
+ 
 app.listen(3000, ()=>console.log('Servidor escuchando en http://localhost:3000'))
