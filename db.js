4674f02723b361dbcf863559246919f8eb710125
Sebastián Aguilera Madariaga <sebaguimad.94@gmail.com>
2023-02-10 18:41:14 -0300

    version final

--- db.js
+++ db.js
@@ -13,7 +13,7 @@ const client = new Client({
 
 client.connect(); 
 
-// Para conductores
+// Retorna la lista de todos los conductores.
 const traerConductores = ()=>{
     //para hacerlo funcionar tuve que typear la consulta en el query de postgres 
     // Se debe trabajar como una promesa porque era asíncrono
@@ -32,7 +32,7 @@ const traerConductores = ()=>{
 // llamando a la función
 //traerConductores();
 
-// Para vehiculos
+// Retorna la lista de todos los automóviles.
 const traerVehiculos = () => {
     //para hacerlo funcionar tuve que typear la consulta en el query de postgres 
     // Se debe trabajar como una promesa porque era asíncrono
@@ -46,8 +46,9 @@ const traerVehiculos = () => {
     })
 }
 
-// Para traerConductoresSinVehiculo
-const traerConductoresSinVehiculo= (edad) => {
+
+// Retorna lista de conductores menores de <numero> de años que no tienen automóvil
+const traerConductoresSinVehiculoporEdad = (edad) => {
     return new Promise((resolve, reject) => {
         client.query(`select nombre, edad from conductores c
                         left join automoviles a
@@ -62,9 +63,60 @@ const traerConductoresSinVehiculo= (edad) => {
 }
 
 
+// Retorna lista de automóviles que no tienen conductor y lista de conductores que no tienen automóvil
+const traerSolitos = () => {
+    return new Promise((resolve, reject) => {
+        client.query(`SELECT automoviles.marca AS solito FROM automoviles
+                        LEFT JOIN conductores ON conductores.nombre = automoviles.nombre_conductor
+                        WHERE conductores.nombre IS NULL
+                        UNION
+                        SELECT conductores.nombre AS nombre_conductor FROM conductores
+                        LEFT JOIN automoviles ON conductores.nombre = automoviles.nombre_conductor
+                        WHERE automoviles.nombre_conductor IS NULL`, (err, result) => {
+            if(err){
+                return reject(err)
+            }
+            resolve(result.rows)
+        })
+    })
+}
+
+
+// Retorna el automóvil con la patente <string> y los datos de su conductor (si es que tiene)
+const traerVehiculoporPatente = (patente) => {
+    return new Promise((resolve, reject) => {
+        client.query(`SELECT automoviles.marca AS marcas_auto, patente, nombre_conductor FROM automoviles
+                        LEFT JOIN conductores ON conductores.nombre = automoviles.nombre_conductor
+                        WHERE patente = ${patente}`, (err, result) => {
+            if(err){
+                return reject(err)
+            }
+            resolve(result.rows)
+        })
+    })
+}
+
+// Retorna la lista de automóviles con patente empezada en la letra <letra> y los datos de su conductor (si es que tiene)
+const traerVehiculoporPatenteyLetrainicial = (letra) => {
+    return new Promise((resolve, reject) => {
+        client.query(`SELECT automoviles.marca AS marcas_auto, patente, nombre_conductor FROM automoviles
+                        LEFT JOIN conductores ON conductores.nombre = automoviles.nombre_conductor
+                        WHERE automoviles.patente = ${letra}%`, (err, result) => {
+            if(err){
+                return reject(err)
+            }
+            resolve(result.rows)
+        })
+    })
+}
+
+
 // exportamos la función para poder consumirla en index.js haciéndolo como ecmascript 5, en 6 se hace con import y export
 module.exports={
     traerConductores,
     traerVehiculos,
-    traerConductoresSinVehiculo
+    traerConductoresSinVehiculoporEdad,
+    traerSolitos,
+    traerVehiculoporPatente,
+    traerVehiculoporPatenteyLetrainicial
 }
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 // destructuring se llaman los {}
-const { traerConductores, traerVehiculos, traerConductoresSinVehiculo } = require('./db.js')
+const { traerConductores, traerVehiculos, traerConductoresSinVehiculoporEdad, traerSolitos, traerVehiculoporPatente } = require('./db.js')
 
 app.get('/', function (req, res) {
   res.send('Ejercicio 2')
 })
 
 
-// RETORNA LISTA DE TODOS LOS CONDUCTORES
+// Retorna la lista de todos los conductores.
 app.get('/conductores', async function (req, res) {
     let conductores = await traerConductores();
     if(!conductores){
@@ -23,7 +23,7 @@ app.get('/conductores', async function (req, res) {
     }
 })
   
-// RETORNA LISTA DE TODOS LOS VEHICULOS
+// Retorna la lista de todos los automóviles.
 app.get('/vehiculos', async function(req, res) {
     traerVehiculos().then(vehiculos => {
         let acumulador = "<ol>"
@@ -40,11 +40,10 @@ app.get('/vehiculos', async function(req, res) {
   })
 
 
-// RETORNA LISTA DE CONDUCTORES SIN AUTO
-
+// Retorna lista de conductores menores de <numero> de años que no tienen automóvil
 app.get('/conductoressinauto', async function(req, res) {
     let edad = req.query.edad || 99;
-    traerConductoresSinVehiculo(edad).then(listado => {
+    traerConductoresSinVehiculoporEdad(edad).then(listado => {
 
         if(listado.length == 0){
             return res.send(`<h1>No se encontraron usuarios sin autos con una edad inferior o igual a ${edad}</h1>`)
@@ -61,7 +60,51 @@ app.get('/conductoressinauto', async function(req, res) {
         console.log(error)
         res.status(500).send("Error interno del servidor.");
     })
+  })
 
+
+// con get con ruta /solitos: Retorna lista de automóviles que no tienen conductor y lista de conductores que no tienen automóvil
+app.get('/solitos', async function(req, res) {
+    traerSolitos().then(solitos => {
+        let acumulador = "<ol>"
+        solitos.forEach(solito => {
+            console.log(solito)
+            acumulador+= `<li>${solito.solito}</li>`
+        });
+        acumulador+="</ol>"
+        res.send(acumulador);
+
+    }).catch(error => {
+        console.log(error)
+        res.send('no existen vehiculos en la BD.')
+    })
+  })
+
+
+// Con ruta get /auto retorna la lista de automóviles con patente empezada en la letra <letra> y los datos de su conductor (si es que tiene)
+app.get('/auto', async function(req, res) {
+    let patente = req.query.patente || '';
+    traerVehiculoporPatente(patente).then(listado => {
+
+        if(listado.length == ''){
+            console.log(patente)
+            return res.send(`<h1>No se encontraron usuarios con la patente: ${patente}</h1>`)
+        }
+
+        let acumulador = "<ol>"
+        listado.forEach(conductor => {
+            console.log(conductor)
+            acumulador+= `<li>Nombre: ${conductor.nombre_conductor} - Patente: ${conductor.patente} - Marca: ${conductor.marcas_auto}</li>`
+        });
+
+        acumulador+="</ol>"
+        console.log(acumulador)
+        res.send(acumulador);
+
+    }).catch(error => {
+        console.log(error)
+        res.status(500).send("Error interno del servidor.");
+    })
   })
 
  
